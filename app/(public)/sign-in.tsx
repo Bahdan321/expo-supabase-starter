005f013456f73ff3a5bad853f446970e8429afab
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-04-28 20:38:52 +0200

    initial commit

--- app/(public)/sign-in.tsx
+++ app/(public)/sign-in.tsx
@@ -1,8 +1,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "expo-router";
 import { useForm } from "react-hook-form";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, View, KeyboardAvoidingView, TouchableOpacity } from "react-native";
 import * as z from "zod";
+
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useState } from 'react';
+
 
 import { SafeAreaView } from "@/components/safe-area-view";
 import { Button } from "@/components/ui/button";
@@ -22,6 +27,7 @@ const formSchema = z.object({
 export default function SignIn() {
 	const { signInWithPassword } = useSupabase();
 	const router = useRouter();
+	const [loading, setLoading] = useState(false)
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -32,6 +38,7 @@ export default function SignIn() {
 	});
 
 	async function onSubmit(data: z.infer<typeof formSchema>) {
+		setLoading(true)
 		try {
 			await signInWithPassword(data.email, data.password);
 
@@ -39,71 +46,84 @@ export default function SignIn() {
 		} catch (error: Error | any) {
 			console.log(error.message);
 		}
+		setLoading(false)
 	}
 
 	return (
-		<SafeAreaView className="flex-1 bg-background p-4">
-			<View className="flex-1">
-				<H1 className="self-start">Sign In</H1>
-				<Muted className="self-start mb-5">
-					to continue to Expo Supabase Starter
-				</Muted>
-				<Form {...form}>
-					<View className="gap-4">
-						<FormField
-							control={form.control}
-							name="email"
-							render={({ field }) => (
-								<FormInput
-									label="Email"
-									placeholder="Email"
-									autoCapitalize="none"
-									autoComplete="email"
-									autoCorrect={false}
-									keyboardType="email-address"
-									{...field}
-								/>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="password"
-							render={({ field }) => (
-								<FormInput
-									label="Password"
-									placeholder="Password"
-									autoCapitalize="none"
-									autoCorrect={false}
-									secureTextEntry
-									{...field}
-								/>
-							)}
-						/>
-					</View>
-				</Form>
-			</View>
-			<View className="gap-y-4">
-				<Button
-					size="default"
-					variant="default"
-					onPress={form.handleSubmit(onSubmit)}
-				>
-					{form.formState.isSubmitting ? (
-						<ActivityIndicator size="small" />
-					) : (
-						<Text>Sign In</Text>
-					)}
-				</Button>
-				<Muted
-					className="text-center"
-					onPress={() => {
+		<SafeAreaView className='flex-1 flex justify-around  bg-black'>
+			<KeyboardAvoidingView behavior='padding'>
+				<View className="mx-2">
+					<TouchableOpacity onPress={() => {
 						router.replace("/sign-up");
-					}}
-				>
-					Don't have an account?{" "}
-					<Muted className="text-foreground">Sign up</Muted>
-				</Muted>
-			</View>
-		</SafeAreaView>
+					}}>
+						<Ionicons name='arrow-back' color='gray' size={30} />
+					</TouchableOpacity>
+				</View>
+				<View className='space-y-0' >
+					<Text style={{ fontSize: wp('18'), }} className='text-center text-1xl font-bold text-gray-700'>
+						Welcome back
+					</Text>
+					<View className='flex-row'>
+						<Text style={{ fontSize: wp('14'), }} className='text-left text-1xl font-bold text-gray-700 m-3'>
+							to
+						</Text>
+						<Text style={{ fontSize: wp('14'), }} className='text-left text-1xl font-extrabold text-gray-400 m-3'>
+							Epidemo
+						</Text>
+					</View>
+					<Form {...form}>
+						<View className="m-2">
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormInput
+										label="Email"
+										placeholder="Email"
+										autoCapitalize="none"
+										autoComplete="email"
+										autoCorrect={false}
+										keyboardType="email-address"
+										{...field}
+									/>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormInput
+										label="Password"
+										placeholder="Password"
+										autoCapitalize="none"
+										autoCorrect={false}
+										secureTextEntry
+										{...field}
+									/>
+								)}
+							/>
+						</View>
+					</Form>
+					{loading ? <ActivityIndicator size='large' color='#0000ff' />
+						:
+						<>
+							<View className="m-4">
+								<Button
+									size="default"
+									variant="default"
+									onPress={form.handleSubmit(onSubmit)}
+								>
+									{form.formState.isSubmitting ? (
+										<ActivityIndicator size="small" />
+									) : (
+										<Text>Войти</Text>
+									)}
+								</Button>
+							</View>
+						</>
+					}
+				</View>
+			</KeyboardAvoidingView>
+		</SafeAreaView >
 	);
 }
--- app/(public)/sign-up.tsx
+++ app/(public)/sign-up.tsx
@@ -1,8 +1,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "expo-router";
 import { useForm } from "react-hook-form";
-import { ActivityIndicator, View } from "react-native";
+import { View, Image, TouchableOpacity, TextInput, ActivityIndicator, KeyboardAvoidingView } from "react-native";
 import * as z from "zod";
+
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
 
 import { SafeAreaView } from "@/components/safe-area-view";
 import { Button } from "@/components/ui/button";
@@ -13,34 +18,61 @@ import { useSupabase } from "@/context/supabase-provider";
 
 const formSchema = z
 	.object({
-		email: z.string().email("Please enter a valid email address."),
+		email: z.string().email("Пожалуйста, введите действительный адрес электронной почты"),
 		password: z
 			.string()
-			.min(8, "Please enter at least 8 characters.")
-			.max(64, "Please enter fewer than 64 characters.")
+			.min(8, "Минималка 8 символов")
+			.max(64, "Максималка 64 символа")
 			.regex(
 				/^(?=.*[a-z])/,
-				"Your password must have at least one lowercase letter.",
+				"Ваш пароль должен содержать хотя бы одну строчную букву",
 			)
 			.regex(
 				/^(?=.*[A-Z])/,
-				"Your password must have at least one uppercase letter.",
+				"Ваш пароль должен содержать хотя бы одну заглавную букву",
 			)
-			.regex(/^(?=.*[0-9])/, "Your password must have at least one number.")
+			.regex(/^(?=.*[0-9])/, "Ваш пароль должен содержать хотя бы одну цифру")
 			.regex(
 				/^(?=.*[!@#$%^&*])/,
-				"Your password must have at least one special character.",
+				"Ваш пароль должен содержать хотя бы один специальный символ",
 			),
-		confirmPassword: z.string().min(8, "Please enter at least 8 characters."),
+		confirmPassword: z.string().min(8, "Пожалуйста, введите не менее 8 символов"),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
-		message: "Your passwords do not match.",
+		message: "Ваши пароли не совпадают",
 		path: ["confirmPassword"],
 	});
 
 export default function SignUp() {
 	const { signUp } = useSupabase();
 	const router = useRouter();
+	const [image, SetImage] = useState();
+	const [loading, setLoading] = useState(false)
+
+	const pickImage = async () => {
+		try {
+			await ImagePicker.requestCameraPermissionsAsync();
+			let result = await ImagePicker.launchCameraAsync({
+				cameraType: ImagePicker.CameraType.front,
+				allowsEditing: true,
+				aspect: [1, 1],
+				quality: 1,
+			});
+			if (!result.canceled) {
+				await saveImage(result.assets[0].uri)
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const saveImage = async (image: any) => {
+		try {
+			SetImage(image)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -52,6 +84,7 @@ export default function SignUp() {
 	});
 
 	async function onSubmit(data: z.infer<typeof formSchema>) {
+		setLoading(true)
 		try {
 			await signUp(data.email, data.password);
 
@@ -59,85 +92,121 @@ export default function SignUp() {
 		} catch (error: Error | any) {
 			console.log(error.message);
 		}
+		setLoading(false)
 	}
 
 	return (
-		<SafeAreaView className="flex-1 bg-background p-4">
-			<View className="flex-1">
-				<H1 className="self-start">Sign Up</H1>
-				<Muted className="self-start mb-5">
-					to continue to Expo Supabase Starter
-				</Muted>
-				<Form {...form}>
-					<View className="gap-4">
-						<FormField
-							control={form.control}
-							name="email"
-							render={({ field }) => (
-								<FormInput
-									label="Email"
-									placeholder="Email"
-									autoCapitalize="none"
-									autoComplete="email"
-									autoCorrect={false}
-									keyboardType="email-address"
-									{...field}
-								/>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="password"
-							render={({ field }) => (
-								<FormInput
-									label="Password"
-									placeholder="Password"
-									autoCapitalize="none"
-									autoCorrect={false}
-									secureTextEntry
-									{...field}
-								/>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="confirmPassword"
-							render={({ field }) => (
-								<FormInput
-									label="Confirm Password"
-									placeholder="Confirm password"
-									autoCapitalize="none"
-									autoCorrect={false}
-									secureTextEntry
-									{...field}
-								/>
-							)}
-						/>
+		<SafeAreaView className='flex-1 flex justify-around  bg-black'>
+			<KeyboardAvoidingView behavior='padding'>
+				<View className='space-y-0' >
+					<Text style={{ fontSize: wp('18'), }} className='text-center text-1xl font-bold text-gray-700'>
+						Welcome to
+					</Text>
+					<Text style={{ fontSize: wp('15'), }} className='text-left text-1xl font-extrabold text-gray-400 m-3'>
+						Epidemo
+					</Text>
+					<View className="p-3 rounded-xl space-y-1 m-4">
+						<TouchableOpacity onPress={pickImage}>
+							{image ? <Image source={{ uri: image }} className="w-40 h-40 rounded-full mx-auto  bg-transparent" />
+								:
+								<Image source={require('../../assets/images/7.png')} className="w-40 h-40 rounded-full mx-auto bg-transparent" />
+							}
+						</TouchableOpacity>
 					</View>
-				</Form>
-			</View>
-			<View className="gap-y-4">
-				<Button
-					size="default"
-					variant="default"
-					onPress={form.handleSubmit(onSubmit)}
-				>
-					{form.formState.isSubmitting ? (
-						<ActivityIndicator size="small" />
-					) : (
-						<Text>Sign Up</Text>
-					)}
-				</Button>
-				<Muted
-					className="text-center"
-					onPress={() => {
-						router.replace("/sign-in");
-					}}
-				>
-					Already have an account?{" "}
-					<Muted className="text-foreground">Sign in</Muted>
-				</Muted>
-			</View>
-		</SafeAreaView>
+					<Form {...form}>
+						<View className="m-2">
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormInput
+										label="Email"
+										placeholder="Email"
+										autoCapitalize="none"
+										autoComplete="email"
+										autoCorrect={false}
+										keyboardType="email-address"
+										{...field}
+									/>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormInput
+										label="Password"
+										placeholder="Password"
+										autoCapitalize="none"
+										autoCorrect={false}
+										secureTextEntry
+										{...field}
+									/>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="confirmPassword"
+								render={({ field }) => (
+									<FormInput
+										label="Confirm Password"
+										placeholder="Confirm password"
+										autoCapitalize="none"
+										autoCorrect={false}
+										secureTextEntry
+										{...field}
+									/>
+								)}
+							/>
+						</View>
+					</Form>
+					{loading ? <ActivityIndicator size='large' color='#0000ff' />
+						:
+						<>
+							<View className="gap-y-4 m-4">
+								<Button
+									size="default"
+									variant="default"
+									onPress={form.handleSubmit(onSubmit)}
+								>
+									{form.formState.isSubmitting ? (
+										<ActivityIndicator size="small" />
+									) : (
+										<Text>Зарегистрироваться</Text>
+									)}
+								</Button>
+								<Muted
+									className="text-center"
+									onPress={() => {
+										router.replace("/sign-in");
+									}}
+								>
+									Already have an account?{" "}
+									<Muted className="text-background">Sign in</Muted>
+								</Muted>
+								<View>
+									<View className='flex-row justify-center'>
+										<Text style={{ fontSize: wp(2.5), }} className='text-center text-gray-400 m-1 font-bold'>
+											При регистрации вы соглашаетесь с
+										</Text>
+										<TouchableOpacity onPress={() => { }}>
+											<Text style={{ fontSize: wp(2.5), }} className='text-center text-gray-400 m-1 font-bold'>
+												условиями использования
+											</Text>
+										</TouchableOpacity>
+									</View>
+									<Text style={{ fontSize: wp(2.5), }} className='text-center text-gray-600 m-2 font-bold'>
+										При регистрации указывайте ваше настоящее фото, для лучшего опыта
+									</Text>
+									<Text style={{ fontSize: wp(2.5), }} className='text-center text-gray-600 font-bold'>
+										И без шалунства:3
+									</Text>
+								</View>
+							</View>
+						</>
+					}
+				</View>
+			</KeyboardAvoidingView>
+		</SafeAreaView >
 	);
 }
